82ec2475deb9f10d535b4ce91a335a7c24ec1dec
Vardhan <164505778+Guru2345@users.noreply.github.com>
2025-02-28 21:44:12 +0530

    Create Rev triangle.java

---
+++ Rev triangle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Triangle{
+public static void main(String args[]){
+int n=5;
+for(int i=1;i<=n;i++){
+for(int j=i;j<=5;j++){
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
