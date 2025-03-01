eeaacf8a438198c985dcf3c888dbf8e4f3d71c13
Vardhan <164505778+Guru2345@users.noreply.github.com>
2025-03-01 22:47:00 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Numbertri {
+    public static void main(String args[]) {
+        int n = 5;    
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n -1; j++) {
+                System.out.print(i);
+            }
+          System.out.println("");
+        }
+    }
+}
+1111
+2222
+3333
+4444
