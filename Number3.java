1e4c7c7079eec026372e92997a704c497310a694
Vardhan <164505778+Guru2345@users.noreply.github.com>
2025-03-16 10:15:42 +0530

    Create Number3.java

---
+++ Number3.java
@@ -0,0 +1,16 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=4;j>=i;j--){
+             System.out.print(j+"");   
+            }
+        
+        System.out.println();}
+    }
+}
+4321
+432
+43
+4
