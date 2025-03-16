b2e9b677f0a96ba3a75e86ce0c4fe0d4417c82aa
Vardhan <164505778+Guru2345@users.noreply.github.com>
2025-03-16 10:11:07 +0530

    Create Number2.java

---
+++ Number2.java
@@ -0,0 +1,16 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+             System.out.print(i+"");   
+            }
+        
+        System.out.println();}
+    }
+}
+1111
+2222
+3333
+4444
