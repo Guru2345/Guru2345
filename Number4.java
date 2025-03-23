9c65875ceedb765adf539fe0f7b611dfb7b1ca2d
Vardhan <164505778+Guru2345@users.noreply.github.com>
2025-03-23 12:59:26 +0530

    Create Number4.java

---
+++ Number4.java
@@ -0,0 +1,32 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int count=1;
+        for(int i=1;i<=4;i++){
+            for( int j=1;j<=4;j++){
+                System.out.print(count );
+                count++;
+              
+            }
+        System.out.println("");
+        
+        if(  i%2==0){
+            int a=count+4;
+            System.out.print(a);
+            a--;
+        }
+        else{
+            int a=count+4;
+           System.out .print(a);
+           a++;
+        }
+        }
+    }
+}
+1234
+95678
+139101112
+1713141516
+21
